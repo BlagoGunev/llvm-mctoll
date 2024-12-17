c57c19125709fcb3fc62ec686b17dac178be2b0d
Blago Gunev <bgunev@gmail.com>
2024-12-17 17:30:27 +0200

    Add method signatures for X86ValueSetAnalysis

--- X86/X86ValueSetAnalysis.h
+++ X86/X86ValueSetAnalysis.h
@@ -15,17 +15,44 @@
 #define LLVM_TOOLS_LLVM_MCTOLL_X86_X86VALUESETANALYSIS_H
 
 #include "X86MachineInstructionRaiser.h"
+#include "ReducedIntervalCongruence.h"
 
 namespace llvm {
 namespace mctoll {
+
+using AlocType = uint32_t;
+using MemRgnType = uint32_t;
+
+using RgnRICPair = std::pair<MemRgnType, ReducedIntervalCongruence>;
+using ValueSet = std::unordered_set<RgnRICPair>;
+
+// map key : a-loc
+// 
+// value - (map(value set, RgnRICPair))
+using AlocToVSMap = std::unordered_map<AlocType, ValueSet *>;
+
+using FPSetsPair = std::pair<std::unordered_set<AlocType>, std::unordered_set<AlocType>>;
 
 class X86ValueSetAnalysis {
 public:
   X86ValueSetAnalysis() = delete;
   X86ValueSetAnalysis(X86MachineInstructionRaiser *);
 
+  bool containsValue(AlocType aloc, int64_t value);
+  bool isSubsetOf(ValueSet *left, ValueSet *right);
+  ValueSet *intersectionVS(ValueSet *left, ValueSet *right);
+  ValueSet *unionVS(ValueSet *left, ValueSet *right);
+  ValueSet *widenVS(ValueSet *left, ValueSet *right);
+  ValueSet *adjustVS(ValueSet *vs, int64_t value);
+  FPSetsPair fpSets(ValueSet *vs, int64_t size);
+  ValueSet *removeLowerBounds(ValueSet *vs);
+  ValueSet *removeUpperBounds(ValueSet *vs);
+
+
 private:
   X86MachineInstructionRaiser *X86MIRaiser;
+
+  AlocToVSMap alocToVSMap;
   
 };
 
